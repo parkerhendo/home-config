c6af1447a74fa3479676a43dec73d98907575c36
Parker Henderson <parker@parkerhendo.com>
2022-10-15 20:27:23 -0700

    migrated config to lua

---
+++ nvim/lua/keymaps.lua
@@ -0,0 +1,52 @@
+local keymap = vim.api.nvim_set_keymap
+
+vim.g.mapleader = ","
+
+-- init.lua management
+keymap("n", "<leader>v", ":e $MYVIMRC<cr>", {})
+keymap("n", "<leader>V", ":source $MYVIMRC<cr>", {})
+keymap("n", "<leader>pi", ":source $MYVIMRC<cr>:PackerSync<cr>", {})
+
+-- quick write and quit
+keymap("n", "<leader>w", ":w<cr>", {})
+keymap("n", "<leader>q", ":q<cr>", {})
+
+-- nvim-tree
+keymap("n", "<leader>e", ":NvimTreeToggle<cr>", {})
+
+-- navigating splits
+keymap("n", "<M-j>", "<C-w>j", {})
+keymap("n", "<M-k>", "<C-w>k", {})
+keymap("n", "<M-h>", "<C-w>h", {})
+keymap("n", "<M-l>", "<C-w>l", {})
+
+-- Move lines up/down (visual mode)
+keymap("v", "J", ":m '>+1<cr>gv=gv", {})
+keymap("v", "K", ":m '<-2<cr>gv=gv", {})
+
+-- Telescope
+vim.cmd([[
+nnoremap <leader>f <cmd>lua require('telescope.builtin').find_files()<cr>
+nnoremap <leader>g <cmd>lua require('telescope.builtin').live_grep()<cr>
+nnoremap <space> <cmd>lua require('telescope.builtin').buffers()<cr>
+]])
+
+-- Trouble
+vim.api.nvim_set_keymap("n", "<leader>xx", "<cmd>Trouble<cr>",
+  {silent = true, noremap = true}
+)
+vim.api.nvim_set_keymap("n", "<leader>xw", "<cmd>Trouble workspace_diagnostics<cr>",
+  {silent = true, noremap = true}
+)
+vim.api.nvim_set_keymap("n", "<leader>xd", "<cmd>Trouble document_diagnostics<cr>",
+  {silent = true, noremap = true}
+)
+vim.api.nvim_set_keymap("n", "<leader>xl", "<cmd>Trouble loclist<cr>",
+  {silent = true, noremap = true}
+)
+vim.api.nvim_set_keymap("n", "<leader>xq", "<cmd>Trouble quickfix<cr>",
+  {silent = true, noremap = true}
+)
+vim.api.nvim_set_keymap("n", "gr", "<cmd>Trouble lsp_references<cr>",
+  {silent = true, noremap = true}
+)
---
+++ nvim/lua/plugins.lua
@@ -0,0 +1,66 @@
+return require('packer').startup(function(use)
+  use {
+    'nvim-tree/nvim-tree.lua',
+    requires = {
+      'nvim-tree/nvim-web-devicons' 
+    },
+    tag = 'nightly'
+  }
+  use {
+    'nvim-lualine/lualine.nvim',
+    requires = { 'nvim-tree/nvim-web-devicons', opt = true }
+  }
+	use {
+    'nvim-treesitter/nvim-treesitter',
+    run = ':TSUpdate'
+  }
+  use {
+    'nvim-telescope/telescope.nvim',
+    requires = { {'nvim-lua/plenary.nvim'} }
+  }
+  use {
+    'numToStr/Comment.nvim',
+    config = function ()
+      require('Comment').setup()
+    end
+  }
+  use {
+    "folke/trouble.nvim",
+    requires = "kyazdani42/nvim-web-devicons",
+    config = function()
+      require("trouble").setup {
+        -- your configuration comes here
+        -- or leave it empty to use the default settings
+        -- refer to the configuration section below
+      }
+    end
+  }
+	use 'wbthomason/packer.nvim'
+	use 'jasonlong/nord-vim'
+  use "nvim-lua/plenary.nvim"
+  use 'nvim-tree/nvim-web-devicons'
+  use 'JoosepAlviste/nvim-ts-context-commentstring'
+  use 'nvim-treesitter/playground'
+  use 'tpope/vim-surround'
+  use 'jiangmiao/auto-pairs'
+  use 'lewis6991/impatient.nvim'
+  use 'karb94/neoscroll.nvim'
+  use 'ggandor/leap.nvim'
+  use 'windwp/nvim-ts-autotag'
+  use 'github/copilot.vim'
+
+  -- Git
+  use 'APZelos/blamer.nvim'
+  use 'akinsho/toggleterm.nvim'
+  use 'lewis6991/gitsigns.nvim'
+  
+  -- Autocomplete
+  use 'neovim/nvim-lspconfig'
+  use 'hrsh7th/cmp-nvim-lsp'
+  use 'hrsh7th/cmp-buffer'
+  use 'hrsh7th/cmp-path'
+  use 'hrsh7th/cmp-cmdline'
+  use 'hrsh7th/nvim-cmp'
+  use 'L3MON4D3/LuaSnip'
+  use 'saadparwaiz1/cmp_luasnip'
+end)
