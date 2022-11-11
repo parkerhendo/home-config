8e01a18a5733db2d45b68a3c37c5a57248e0e7bf
Parker Henderson <parker@parkerhendo.com>
2022-11-10 21:25:33 -0800

    reduced timeout of yank highlight

--- nvim/lua/config.lua
+++ nvim/lua/config.lua
@@ -47,7 +47,7 @@ vim.cmd [[lcd $PWD]]
 vim.cmd([[
 augroup highlight_yank
     autocmd!
-    au TextYankPost * silent! lua vim.highlight.on_yank{higroup="IncSearch", timeout=500}
+    au TextYankPost * silent! lua vim.highlight.on_yank{higroup="IncSearch", timeout=100}
 augroup END
 ]])
 
