729b0c9c385ad9afecf41b97dc362a3468f93162
Parker Henderson <parker@parkerhendo.com>
2023-09-18 22:32:02 -0700

    git-conflict config

--- nvim/lua/plugins.lua
+++ nvim/lua/plugins.lua
@@ -41,7 +41,15 @@ return require('packer').startup(function(use)
     'akinsho/git-conflict.nvim',
     tag = "*",
     config = function()
-      require('git-conflict').setup()
+      require('git-conflict').setup {
+        default_mappings = false, -- disable buffer local mapping created by this plugin
+        disable_diagnostics = false, -- This will disable the diagnostics in a buffer whilst it is conflicted
+        list_opener = "copen", -- command or function to open the conflicts list
+        highlights = { -- They must have background color, otherwise the default color will be used
+          incoming = "DiffText",
+          current = "DiffAdd",
+        },
+      }
     end
   }
   use "nvim-lua/plenary.nvim"
