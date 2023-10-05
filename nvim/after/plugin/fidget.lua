1019755a82cff16b784c81ff14c2a24a5a6d6e80
Parker Henderson <parker@parkerhendo.com>
2023-10-05 08:09:41 -0700

    update fidget theme

--- nvim/after/plugin/fidget.lua
+++ nvim/after/plugin/fidget.lua
@@ -1,6 +1,13 @@
-require"fidget".setup({
-  window = {
-    blend = 100,
-    zindex = 2,
-  },
+require("fidget").setup({
+	window = {
+		relative = "editor", -- where to anchor, either "win" or "editor"
+		blend = 0, -- &winblend for the window
+		zindex = 2, -- the zindex value for the window
+		border = "solid",
+	},
 })
+
+vim.cmd([[
+  highlight FidgetTask ctermfg=110 guifg=#FFFFFF
+  highlight FidgetTitle ctermfg=110 guifg=#6cb6eb
+]])
--- nvim/after/plugin/lualine.lua
+++ nvim/after/plugin/lualine.lua
@@ -1,57 +1,57 @@
 -- lualine
-nord1 = "#232730"
-nord2 = "#2e3440"
-nord3 = "#3b4252"
-nord5 = "#e5e9f0"
-nord6 = "#eceff4"
-nord7 = "#8fbcbb"
-nord8 = "#88c0d0"
-nord14 = "#ebcb8b"
+local nord1 = "#232730"
+local nord2 = "#2e3440"
+local nord3 = "#3b4252"
+local nord5 = "#e5e9f0"
+local nord6 = "#eceff4"
+local nord7 = "#8fbcbb"
+local nord8 = "#88c0d0"
+local nord14 = "#ebcb8b"
 
-local nord = require'lualine.themes.nord'
+local custom_nord = require("lualine.themes.nord")
 
-nord.normal.a.fg = nord1
-nord.normal.a.bg = nord7
-nord.normal.b.fg = nord5
-nord.normal.b.bg = nord2
-nord.normal.c.fg = nord5
-nord.normal.c.bg = nord3
+custom_nord.normal.a.fg = nord1
+custom_nord.normal.a.bg = nord7
+custom_nord.normal.b.fg = nord5
+custom_nord.normal.b.bg = nord2
+custom_nord.normal.c.fg = nord5
+custom_nord.normal.c.bg = nord3
 
-nord.insert.a.fg = nord1
-nord.insert.a.bg = nord6
+custom_nord.insert.a.fg = nord1
+custom_nord.insert.a.bg = nord6
 
-nord.visual.a.fg = nord1
-nord.visual.a.bg = nord7
+custom_nord.visual.a.fg = nord1
+custom_nord.visual.a.bg = nord7
 
-nord.replace.a.fg = nord1
-nord.replace.a.bg = nord13
+custom_nord.replace.a.fg = nord1
+custom_nord.replace.a.bg = nord14
 
-nord.inactive.a.fg = nord1
-nord.inactive.a.bg = nord7
-nord.inactive.b.fg = nord5
-nord.inactive.b.bg = nord1
-nord.inactive.c.fg = nord5
-nord.inactive.c.bg = nord2
+custom_nord.inactive.a.fg = nord1
+custom_nord.inactive.a.bg = nord7
+custom_nord.inactive.b.fg = nord5
+custom_nord.inactive.b.bg = nord1
+custom_nord.inactive.c.fg = nord5
+custom_nord.inactive.c.bg = nord2
 
-require('lualine').setup {
-  options = {
-    theme = nord,
-    component_separators = { left = '·'},
-  },
-  sections = {
-    lualine_a = {'mode'},
-    lualine_b = {'branch', 'diff', 'diagnostics'},
-    lualine_c = { {'filename', path = 1} },
-    lualine_x = {'filetype'},
-    lualine_y = {'progress'},
-    lualine_z = {'location'}
-  },
-  inactive_sections = {
-    lualine_a = {},
-    lualine_b = {},
-    lualine_c = { {'filename', path = 1} },
-    lualine_x = {'location'},
-    lualine_y = {},
-    lualine_z = {}
-  }
-}
+require("lualine").setup({
+	options = {
+		theme = custom_nord,
+		component_separators = { left = "·" },
+	},
+	sections = {
+		lualine_a = { "mode" },
+		lualine_b = { "branch", "diff", "diagnostics" },
+		lualine_c = { { "filename", path = 1 } },
+		lualine_x = { "filetype" },
+		lualine_y = { "progress" },
+		lualine_z = { "location" },
+	},
+	inactive_sections = {
+		lualine_a = {},
+		lualine_b = {},
+		lualine_c = { { "filename", path = 1 } },
+		lualine_x = { "location" },
+		lualine_y = {},
+		lualine_z = {},
+	},
+})
