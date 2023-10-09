a34c706d8a56f5d68a1e10864898bc8f8abeaf45
Parker Henderson <parker@parkerhendo.com>
2023-10-08 22:09:33 -0700

    increase default nvim-tree width

--- nvim/after/plugin/nvim_tree.lua
+++ nvim/after/plugin/nvim_tree.lua
@@ -1,18 +1,19 @@
-require('nvim-tree').setup({
-  git = {
-    ignore = false,
-  },
-  filters = {
-    dotfiles = false,
-    custom = { "^.git$" },
-  },
-  update_focused_file = {
-    enable  = true
-  },
-  view = {
-    float = {
-      enable = false,
-      quit_on_focus_loss = true
-    }
-  }
+require("nvim-tree").setup({
+	git = {
+		ignore = false,
+	},
+	filters = {
+		dotfiles = false,
+		custom = { "^.git$" },
+	},
+	update_focused_file = {
+		enable = true,
+	},
+	view = {
+		width = 45,
+		float = {
+			enable = false,
+			quit_on_focus_loss = true,
+		},
+	},
 })
