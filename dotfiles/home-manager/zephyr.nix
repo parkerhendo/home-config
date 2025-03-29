765e1aca488e40ed539a0100d98f585a83fa9024
Parker Henderson <parker@parkerhendo.com>
2025-03-29 10:18:11 -0700

    add zephyr config (#14)
    * add zephyr config
    
    * setup new laptop

---
+++ dotfiles/home-manager/zephyr.nix
@@ -0,0 +1,45 @@
+{ pkgs, ... }: let
+  unstable = import <nixpkgs-unstable> {
+    config.allowUnfree = true;
+  };
+in {
+  home.username = "parker";
+  home.homeDirectory = "/Users/parker";
+  home.stateVersion = "22.11";
+
+  home.packages = with pkgs; [
+    bat
+    coreutils
+    darwin.trash
+    fd
+    ffmpeg
+    fzf
+    gh
+    git
+    lazygit
+    neovim
+    neofetch
+    nixfmt
+    parallel
+    ripgrep
+    rustup
+    skhd
+    tmux
+    tree
+    yabai
+  ];
+
+  programs.git = {
+    enable = true;
+    includes = [{ path = "~/.gitconfig"; }];
+  };
+
+  programs.home-manager.enable = true;
+
+  programs.nix-index-database.comma.enable = true;
+
+  programs.direnv = {
+    enable = true;
+    nix-direnv.enable = true;
+  };
+}
