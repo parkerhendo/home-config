6e15a7d191dd3622c0075a9452f54921d34b0429
Parker Henderson <parker@parkerhendo.com>
2026-07-24 09:34:18 -0700

    make dotfile visibility toggable in nvim

--- dotfiles/nvim/lua/parkerhendo/plugins/oil.lua
+++ dotfiles/nvim/lua/parkerhendo/plugins/oil.lua
@@ -107,20 +107,20 @@ return {
       },
 
       view_options = {
+        show_hidden = false,
         is_hidden_file = function(name, bufnr)
-          local dir = require("oil").get_current_dir(bufnr)
           local is_dotfile = vim.startswith(name, ".") and name ~= ".."
-          -- if no local directory (e.g. for ssh connections), just hide dotfiles
-          if not dir then
-            return is_dotfile
-          end
-          -- dotfiles are considered hidden unless tracked
           if is_dotfile then
-            return not git_status[dir].tracked[name]
-          else
-            -- Check if file is gitignored
-            return git_status[dir].ignored[name]
+            return true
           end
+
+          local dir = require("oil").get_current_dir(bufnr)
+          if not dir then
+            return false
+          end
+
+          -- Check if file is gitignored
+          return git_status[dir].ignored[name]
         end,
       },
     })
